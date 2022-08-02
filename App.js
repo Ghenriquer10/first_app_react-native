0c345c53a5187e2236bd2e38589147d0e09fbc15
Gustavo Henrique <ghenriquer10@gmail.com>
2022-08-02 18:59:28 -0300

    Aprendendo react native

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+export default function App(){
+
+  const [nome, setNome] = useState('Gustavo');
+
+  function handleButton(){
+    alert('VAMOS CODAR!')
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Iniciando com React Native</Text>
+      <Text style={[styles.title, styles.text]}>Hello World, {nome}!</Text>
+      <TouchableOpacity style={styles.button} onPress={handleButton}>
+        <Text style={styles.buttonText}>Clique!</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#121212",
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff',
+
   },
-});
+  title: {
+    color: "#fff",
+    fontSize: 30,
+    margin: 10,
+  },
+
+  text:{
+    fontSize: 15,
+    color: 'red'
+  },
+  button:{
+    height: 30,
+    width: 150,
+    backgroundColor:'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20
+  }, 
+  buttonText:{
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+})
